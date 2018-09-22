9ecc06cf7272a5ddd71a6cd3c61416040b12da3b
Maggie <maggiemxu11@gmail.com>
2018-09-22 10:46:16 +1000

    added flask

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import sys
 import os
 
 app = Flask(__name__)
+
+
+def testPrintCourses():
+    courses = ["MATH1131", "MATH1231", "COMP1531", "COMP1521"]
+    return render_template('index.html', courses = courses)
