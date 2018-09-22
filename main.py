ca9997ddff7a9f22d768183a5bc9934ecd2b2222
Maggie <maggiemxu11@gmail.com>
2018-09-22 14:27:51 +1000

    cleaning up getprereqs codes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def testPrintCourses():
-    courses = ["MATH1131", "MATH1231", "COMP1531", "COMP1521"]
+    courses = ["MATH1131", "MATH1231", "COMP1531", "COMP1521"] # should be list of course objects
     return render_template('index.html', courses = courses)
 
 
