e585e2c4720cea50259b1e2ff406e54056fdaf6c
Maggie <maggiemxu11@gmail.com>
2018-09-22 11:40:48 +1000

    testing

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-
-from flask import *
+from flask import Flask, app, render_template, url_for, redirect, session, request
 import json
 import sys
 import os
 
 app = Flask(__name__)
 
-
+@app.route('/', methods=['GET', 'POST'])
 def testPrintCourses():
     courses = ["MATH1131", "MATH1231", "COMP1531", "COMP1521"]
     return render_template('index.html', courses = courses)
+
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5001)
