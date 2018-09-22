cbedd4cc17a848c849e65f5be22baedc498214de
AndyLiu11037 <z5161216@unsw.edu.au>
2018-09-22 11:40:35 +1000

    no new lines

--- webScrapeTest.py
+++ webScrapeTest.py
@@ -6,13 +6,19 @@ def remove_html_tags(text):
     """Remove html tags from a string"""
     
     clean = re.compile('<.*?>')
-    return re.sub(clean, '', text)
-    
+    clean = re.sub(clean, '', text)
+    clean = clean.replace("\n", '')
+    return clean
+
 page = urlopen("https://www.handbook.unsw.edu.au/undergraduate/specialisations/2019/COMPA1")
 soup = BeautifulSoup(page, 'html.parser')
 data = []
 data = soup.find_all("div", "m-single-course-top-row")
-data_string = str(data[0])
-data_string = remove_html_tags(data_string)
-
+data_string = []
+# [A-Z]{4}[0-9]{4}
+for data_element in data:
+    element_string = str(data_element)
+    data_string.append(remove_html_tags(element_string))
+for element in data_string:
+    print(element)
 
